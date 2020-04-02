ef35e5909d76f3af2c01a8a1252655622cc8a5ae
pjmantoss <joelptoss@gmail.com>
2020-04-02 15:35:11 +0100

    update App.css and Toolbar.js

--- src/components/Toolbar.js
+++ src/components/Toolbar.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 export default function Toolbar(props) {
     return (
-        <div className="toolbar">
+        <div id="toolbar">
             {props.text}
         </div>
     )
